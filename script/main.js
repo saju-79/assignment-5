fa0587d01cf1f790ea1b05449f13c2ba0c8e2084
saju-79 <saju98622@gmail.com>
2025-03-01 14:26:15 -0800

    my-first-main-commit

---
+++ script/main.js
@@ -0,0 +1,7 @@
+
+
+const backButton = document.getElementById('back-button')
+backButton.addEventListener("click" , function(){
+// window.location.href = "i" 
+ window.location.href = "../index.html"
+});
--- script/script.js
+++ script/script.js
@@ -13,6 +13,36 @@ document.getElementById('changeColorBodyBtn').addEventListener('click' , functio
         // console.log('this color code number : ' , fullBodyColorChange())
 })
 
+// big button worke
+const clickButton = document.getElementById('big-button')
+clickButton.addEventListener('click' , function(){
+           window.location.href = "../main.html"
 
 
+});
+// number cromonnoi big 
+    const completedElements = document.querySelectorAll(".complete-button");
+   for(let btn of completedElements){
+    completedElements.addEventListener('click', function(event){
+      const amount = event.target.innerText === "+" ? 1 : -1 ;
+        console.log('hiiiiiiiiiii' , amount)
+    })
+   }
+ 
+    // const quantityElements = document.querySelectorAll(".quantity-button");
+// for (let btn of quantityElements) {
+//   btn.addEventListener("click", function (event) {
+//     const amount = event.target.innerText === "+" ? 1 : -1; // 1 || -1
+//     const quantityEelemnt = document.getElementById("quantity");
+//     const currentQuantity = parseInt(quantityEelemnt.innerText);
+//     const newQuantity = Math.max(0, currentQuantity + amount);
+//     quantityEelemnt.innerText = newQuantity;
+//   });
+// }
+// clear history  deleted
 
+
+document.getElementById("History-clear").addEventListener('click', function(){
+     console.log('hiiiiiiiiiiii');
+     document.getElementById('hidden-display').style.display = "none"
+});
