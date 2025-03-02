ca103cec2c8116e8cb1961e6acba5894704d5860
saju-79 <saju98622@gmail.com>
2025-03-02 05:14:48 -0800

    this-commit

--- script/text.js
+++
@@ -1,9 +0,0 @@
-// const commitText = document.getElementById('hidden-display');
-// const completeButtonClicks = document.querySelectorAll(".complete-button");
-
-//   for (let h = 0; h < completeButtonClicks.length; h++) {
-//     const addButton = completeButtonClicks[h];
-      
-     
-     
-//   }
